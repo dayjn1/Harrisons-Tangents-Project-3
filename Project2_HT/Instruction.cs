40a167b0c8136962ad9d72e0a7ddd9af922e5ab9
dayjn1 <janinenday@gmail.com>
2022-03-03 17:11:51 -0500

    New IS method

--- Project2_HT/Instruction.cs
+++ Project2_HT/Instruction.cs
@@ -31,7 +31,9 @@ namespace Project2_HT
         string DestReg;
         string Reg1;
         string Reg2;
+        int FetchCC, DecodeCC, ExecuteCC, MemoryCC, RegisterCC;
 
+        
 
         public static Dictionary<uint, string> InstructionSet = new Dictionary<uint, string>
         {
@@ -60,6 +62,45 @@ namespace Project2_HT
 
         };
 
+        List<Instruction> InstructionSetNew = new List<Instruction>
+        {
+            new Instruction( 0, "Halt", 1, 1, 1, 0, 0),
+            new Instruction( 1, "LOAD", 1, 2, 1, 3, 1),
+            new Instruction( 2, "STOR", 1, 2, 1, 3, 0),
+            new Instruction( 3,  "ADD", 1, 1, 1, 0, 1),
+            new Instruction( 4, "ADDI", 1, 1, 1, 0, 1),
+            new Instruction( 5,  "SUB", 1, 1, 1, 0, 1),
+            new Instruction( 6, "SUBI", 1, 1, 1, 0, 1),
+            new Instruction( 7,   "BR", 1, 1, 1, 0, 0),
+            new Instruction( 8, "BRLT", 1, 1, 1, 0, 0),
+            new Instruction( 9, "BRLE", 1, 1, 1, 0, 0),
+            new Instruction(10, "BREQ", 1, 1, 1, 0, 0),
+            new Instruction(11, "BRNE", 1, 1, 1, 0, 0),
+            new Instruction(12, "BRGT", 1, 1, 1, 0, 0),
+            new Instruction(13, "BRGE", 1, 1, 1, 0, 0),
+            new Instruction(14,  "AND", 1, 1, 1, 0, 1),
+            new Instruction(15,   "OR", 1, 1, 1, 0, 1),
+            new Instruction(16,  "NOT", 1, 1, 1, 0, 1),
+            new Instruction(17,  "NEG", 1, 1, 1, 0, 1),
+            new Instruction(18,  "ASL", 1, 1, 1, 0, 1),
+            new Instruction(19,  "ASR", 1, 1, 1, 0, 1),
+            new Instruction(20,  "MOV", 1, 1, 1, 0, 1),
+            new Instruction(404, "INVALID", 1, 1, 0, 0, 0)
+
+        };
+
+        public void FindIS()
+        {
+            for(int i = 0; i < InstructionSetNew.Count; i++)
+            {
+                if(this.OpCode == InstructionSetNew[i].OpCode)
+                {
+                    this.Mnemonic = 
+                }    
+            }
+        }
+
+
         /**
         * Method Name: Instruction(int)
         * Method Purpose: Takes an instruction in hex and creates an instruction
@@ -72,7 +113,18 @@ namespace Project2_HT
         */
         public Instruction(int instr)
         {
-            disassemble(instr);
+            Disassemble(instr);
+        }
+
+        public Instruction(uint opcode, string mnemonic, int fetch, int decode, int execute, int memory, int register)
+        {
+            this.OpCode = opcode;
+            this.Mnemonic = mnemonic;
+            this.FetchCC = fetch;
+            this.DecodeCC = decode;
+            this.ExecuteCC = execute;
+            this.MemoryCC = memory;
+            this.RegisterCC = register;
         }
 
 
@@ -86,15 +138,17 @@ namespace Project2_HT
         * <hr>
         * @param int - hex input to covert into instruction
         */
-        public void disassemble(int input)
+        public void Disassemble(int input)
         {
             //separate the opcode
             this.OpCode = (uint)input & 0xFF000000;   //must be unsigned so that r shift (>>) is logical, not arithmetic -JM
             this.OpCode >>= 24;
 
             this.Mnemonic = "";
-            //added condition logic to check for valid parsed instruction (** not finished yet) -H
-            if (InstructionSet.ContainsKey(this.OpCode))
+
+            FindIS();
+
+            /*if (InstructionSet.ContainsKey(this.OpCode))
             {
                 this.Mnemonic = InstructionSet[this.OpCode];
             }
@@ -102,7 +156,7 @@ namespace Project2_HT
             {
                 this.OpCode = 404;                            // Creates illegal instruction
                 this.Mnemonic = InstructionSet[this.OpCode];
-            }
+            }*/
 
             uint rd = (uint)input & 0x00F00000;
             rd >>= 20;
