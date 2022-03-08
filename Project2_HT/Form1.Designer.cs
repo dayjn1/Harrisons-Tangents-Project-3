83eaadbe07e5462d1db9ec06914dedc878e04506
Nataliya Chibizova <60976901+nchibizova@users.noreply.github.com>
2022-03-07 19:07:54 -0500

    Design Changes

--- Project2_HT/Form1.Designer.cs
+++ Project2_HT/Form1.Designer.cs
@@ -70,7 +70,7 @@ namespace Project2_HT
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1010, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(1010, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -82,20 +82,20 @@ namespace Project2_HT
             this.saveToolStripMenuItem});
             this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -110,7 +110,7 @@ namespace Project2_HT
             // DecodeBox
             // 
             this.DecodeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DecodeBox.Location = new System.Drawing.Point(222, 181);
+            this.DecodeBox.Location = new System.Drawing.Point(212, 184);
             this.DecodeBox.Name = "DecodeBox";
             this.DecodeBox.Size = new System.Drawing.Size(100, 34);
             this.DecodeBox.TabIndex = 2;
@@ -134,7 +134,7 @@ namespace Project2_HT
             // RegisterBox
             // 
             this.RegisterBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RegisterBox.Location = new System.Drawing.Point(723, 184);
+            this.RegisterBox.Location = new System.Drawing.Point(729, 184);
             this.RegisterBox.Name = "RegisterBox";
             this.RegisterBox.Size = new System.Drawing.Size(100, 34);
             this.RegisterBox.TabIndex = 5;
@@ -297,7 +297,7 @@ namespace Project2_HT
             this.Controls.Add(this.DecodeBox);
             this.Controls.Add(this.FetchBox);
             this.Controls.Add(this.menuStrip1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Tangents";
             this.Text = "Pipeline Simulator";
             this.menuStrip1.ResumeLayout(false);
