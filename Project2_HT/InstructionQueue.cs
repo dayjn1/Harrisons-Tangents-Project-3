5c19c05a2cc317fe00d7715ae9e47ba7658905b4
MarlowAvery <45161056+MarlowAvery@users.noreply.github.com>
2022-03-23 19:28:59 -0400

    Added an instruction queue class to supplement the use of the instruction stack
    Cooked this up real fast after class and ill re-review it and comment it later

---
+++ Project2_HT/InstructionQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_HT
+{
+    public class InstructionQueue
+    {
+        int linepos = 0;
+        public Queue<IQueueEntry> IQueue = new Queue<IQueueEntry>();
+        public void AddToIQueue(Instruction i)
+        {
+            
+            IQueueEntry myEntry = new IQueueEntry(i, linepos);
+            linepos++;
+        }
+    }
+
+   public struct IQueueEntry
+    {
+       public Instruction i;
+        public int linepos;
+
+        public IQueueEntry(Instruction i, int linepos)
+        {
+            this.i = i;
+            this.linepos = linepos;
+        }
+    }
+}
