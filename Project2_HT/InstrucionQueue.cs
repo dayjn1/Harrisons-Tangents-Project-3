7ca280db6f7690e680a2b09675ce755086acd5ac
Nataliya Chibizova <60976901+nchibizova@users.noreply.github.com>
2022-03-27 19:19:30 -0400

    Avery's changes to Janine's sln

--- Project2_HT/InstrucionQueue.cs
+++ Project2_HT/InstrucionQueue.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// ---------------------------------------------------------------------------
+// File name:                   InstrucionQueue.cs
+// Project name:                Project 3 - Harrison's Tangents
+// ---------------------------------------------------------------------------
+// Creator’s name:              Avery Marlow
+// Edited By:                   Avery Marlow
+// Course-Section:              CSCI-4717
+// Creation Date:               03/25/2022
+// ---------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +17,25 @@ namespace Project3_HT
 {
     class InstrucionQueue
     {
+        int linepos = 0;
+        public Queue<IQueueEntry> IQueue = new Queue<IQueueEntry>();
+        public void AddToIQueue(Instruction i)
+        {
+
+            IQueueEntry myEntry = new IQueueEntry(i, linepos);
+            linepos++;
+        }
+    }
+
+    public struct IQueueEntry
+    {
+        public Instruction i;
+        public int linepos;
+
+        public IQueueEntry(Instruction i, int linepos)
+        {
+            this.i = i;
+            this.linepos = linepos;
+        }
     }
 }
