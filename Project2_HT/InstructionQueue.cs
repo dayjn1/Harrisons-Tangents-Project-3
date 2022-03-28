dd78589b9dd8fc4ec10e82ec03eea3808ddc3223
MarlowAvery <45161056+MarlowAvery@users.noreply.github.com>
2022-03-28 18:49:56 -0400

    Added buffer structures, as well as address unit
    Need Operation Bus data structures to complete Instruction Queue.

--- Project2_HT/InstructionQueue.cs
+++ Project2_HT/InstructionQueue.cs
@@ -1,32 +1,66 @@
-﻿using System;
+﻿// ---------------------------------------------------------------------------
+// File name:                   InstrucionQueue.cs
+// Project name:                Project 3 - Harrison's Tangents
+// ---------------------------------------------------------------------------
+// Creator’s name:              Avery Marlow
+// Edited By:                   Avery Marlow
+// Course-Section:              CSCI-4717
+// Creation Date:               03/25/2022
+// ---------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project2_HT
+namespace Project3_HT
 {
-    public class InstructionQueue
+    class InstructionQueue
     {
-        int linepos = 0;
-        public Queue<IQueueEntry> IQueue = new Queue<IQueueEntry>();
+
+        public Queue<Instruction> IQueue = new Queue<Instruction>();
         public void AddToIQueue(Instruction i)
         {
-            
-            IQueueEntry myEntry = new IQueueEntry(i, linepos);
-            linepos++;
+            IQueue.Enqueue(i);
         }
-    }
 
-   public struct IQueueEntry
-    {
-       public Instruction i;
-        public int linepos;
-
-        public IQueueEntry(Instruction i, int linepos)
+        public void DecueueTheInstruction(Instruction i)
         {
-            this.i = i;
-            this.linepos = linepos;
+            if (i.OpCode == 404) // invelid instruction, stop execution 
+            {
+                IQueue.Dequeue();
+            }
+            else if(i.OpCode == 0) // HALT do not decueue anything after
+            {
+                //call our halt method, wherever that is
+            }
+            else if(i.OpCode == 1) // LOAD -- send it to the address unit --> LOAD buffer
+            {
+                AddressUnit.ProcessAU(i); // go to address unit
+                IQueue.Dequeue();
+            }
+            else if (i.OpCode == 2) // STORE -- send it to the address unit --> RO buffer --> memory unit
+            {
+                AddressUnit.ProcessAU(i); //go to address unit
+                IQueue.Dequeue();
+            }
+            else if(i.OpCode >=3 || i.OpCode <= 20) // goes to the int 
+            {
+                //go to op bus/res station
+            }
+            else if(i.OpCode >= 128 ||i.OpCode <= 131) // goes to floating point
+            {
+                //go to op bus// fp res station
+            }
+            else if(i.OpCode == 132 || i.OpCode == 133)
+            {
+                //go to op bus// fp res station
+            }
+            IQueue.Dequeue();
         }
+
+
     }
+
+   
 }
--- Project2_HT/LoadBuffer.cs
+++ Project2_HT/LoadBuffer.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Project3_HT
 {
-    class LoadBuffer
+    public class LoadBuffer
     {
+        public static Queue<AUTicket> Buffer = new Queue<AUTicket>();
+        public struct AUTicket
+        {
+            public string Name;
+            public String DestReg;
+            public String SourceReg;
+            public AUTicket(String name,String dr, String sr)
+            {
+                Name = name;
+                DestReg = dr;
+                SourceReg = sr;
+            }
+
+        }
+        public static void AddToBuffer(Instruction i)
+        {
+            AUTicket myAU = new AUTicket(i.Mnemonic,i.DestReg, i.Reg1);
+            Buffer.Enqueue(myAU);
+        }
     }
 }
