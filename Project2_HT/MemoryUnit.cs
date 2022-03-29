622d189e9b2447f4a7c31f0385d0b137b3185f28
MarlowAvery <45161056+MarlowAvery@users.noreply.github.com>
2022-03-29 12:43:33 -0400

    Added memory unit. Need CDB, OpBus, and Reorder Buffer

---
+++ Project2_HT/MemoryUnit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Project3_HT;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Project3_HT.LoadBuffer;
+
+public class MemoryUnit
+{
+	static Dictionary<String, String> MemoryEntries = new Dictionary<String, String>();
+	public static void AddToMemUnit(AUTicket myAU)
+    {
+        MemoryEntries.Add(myAU.DestReg, myAU.SourceReg);
+    }
+    public static void Clear()
+    {
+        MemoryEntries.Clear();
+    }
+    public static void SendToCDB()
+    {
+        //will be implemented when CDB gets done
+    }
+}
