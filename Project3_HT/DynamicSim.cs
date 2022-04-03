1efddae8bb2338afc8f81d6e72a67a1649aeb452
Nataliya Chibizova <60976901+nchibizova@users.noreply.github.com>
2022-04-03 17:14:39 -0400

    Classes added to sln
    Class deleted - InstrucionQueue.cs

--- Project3_HT/DynamicSim.cs
+++ Project3_HT/DynamicSim.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
