7805f5cfe605e3635ec7b66fb4b4c1a4686c777f
dayjn1 <janinenday@gmail.com>
2022-04-04 20:27:25 -0400

    Reorder buf changes

--- Project3_HT/ReorderBuffer.cs
+++ Project3_HT/ReorderBuffer.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// ---------------------------------------------------------------------------
+// File name:                   ReorderBuffer.cs
+// Project name:                Project 3 - Harrison's Tangents
+// ---------------------------------------------------------------------------
+// Creator’s name:              Janine Day
+// Edited By:                   Janine Day
+// Course-Section:              CSCI-4717
+// Creation Date:               03/27/2022
+// ---------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +16,45 @@ using System.Threading.Tasks;
 
 namespace Project3_HT
 {
+    /**
+    * Class Name:       ReorderBuffer
+    * Class Purpose:    Handle the reorder buffer
+    *
+    * <hr>
+    * Date created:     03/27/2022
+    * @Janine Day
+    */
     static class ReorderBuffer
     {
         public static Queue<Instruction> ReorderBuf = new Queue<Instruction>();
         public static List<Instruction> Passed = new List<Instruction>();
 
+
+        /**
+        * Method Name:    AddToReorderBuf(Instruction)
+        * Method Purpose: Adds to the reorder buffer queue
+        *
+        * <hr>
+        * Date created: 03/27/2022
+        * @Janine Day
+        * <hr>
+        * @param Instruction to enqueue
+        */
         public static void AddToReorderBuf(Instruction i)
         {
             ReorderBuf.Enqueue(i);
-      
         }
 
+        /**
+        * Method Name:    IsReorderBufFree()
+        * Method Purpose: Determines if there is room in the reorder buffer
+        *
+        * <hr>
+        * Date created: 03/27/2022
+        * @Janine Day
+        * <hr>
+        * @return bool, true if free - false if not
+        */
         public static bool IsReorderBufFree()
         {
             if (ReorderBuf.Count > 5)
