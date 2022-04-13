bc861ac95f63c63ff44f03c9f0ec5e50ff0ff14e
dayjn1 <janinenday@gmail.com>
2022-04-13 18:08:12 -0400

    Address calculation

--- Project3_HT/AddressUnit.cs
+++ Project3_HT/AddressUnit.cs
@@ -1,6 +1,7 @@
 ﻿using Project3_HT;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,14 @@ namespace Project3_HT
             int size = 1;           //size of queue
             if (counter < size)
             {
-                //i.Address = i.Reg1 + i.Imm;
+                bool valid1 = Int32.TryParse(i.DestReg, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out int tempDR);
+                bool valid2 = Int32.TryParse(i.Imm, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out int tempImm);
+
+                if (valid1 && valid2)
+                {
+                    i.Address = (uint)tempDR + (uint)tempImm;
+                }
+
                 //Enqueue
                 AddressUnitQueue.Enqueue(i);
                 counter++;
@@ -26,7 +34,7 @@ namespace Project3_HT
         public static void ProcessAU()
         {
             Instruction i = AddressUnitQueue.Peek();
-            if (i.OpCode == 1)
+            if (i.OpCode == 1 || i.OpCode == 3)
             {
                 // check if there is a space on the 
                 LoadBuffer.AddToLoadBuffer(i);
