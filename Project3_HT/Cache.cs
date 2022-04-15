3183e34da33183bf9d76a09041aba99f82844f9d
Jason Middlebrook <MiddlebrookJF@gmail.com>
2022-04-14 23:31:50 -0400

    Create Cache.cs

---
+++ Project3_HT/Cache.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3_HT
+{
+    internal class Cache
+    {
+        public int SetAssociativity { get; set; }
+        public int TotalSize { get; set; }
+        public int[,] CacheArray { get; set; }
+
+        /// <summary>
+        /// Non-default constructor if you want to specify properties
+        /// </summary>
+        public Cache(int setAssociativity, int totalSize)
+        {
+            SetAssociativity = setAssociativity;
+            TotalSize = totalSize;
+            CacheArray = new int[totalSize / setAssociativity, setAssociativity];
+        }
+
+        /// <summary>
+        /// Default property values assigned based on what kind of cache we decided on
+        /// </summary>
+        public Cache()
+        {
+            SetAssociativity = 4;
+            TotalSize = 16;
+
+            //Create a 2d array; first level is the rows (indices), second is the columns (sets)
+            //Cache will have as many sets per row as the set associativity
+            CacheArray = new int[TotalSize/SetAssociativity, SetAssociativity];
+        }
+
+    }//end class Cache
+}
