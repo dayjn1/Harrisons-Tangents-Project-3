cc9834426bea06344b8475eafbe514267e6453ca
HannahMTaylor <61762557+HannahMTaylor@users.noreply.github.com>
2022-04-18 17:35:10 -0400

    started adding labels to diagram

--- Project3_HT/CacheFourWay.Designer.cs
+++ Project3_HT/CacheFourWay.Designer.cs
@@ -30,7 +30,148 @@ namespace Project2_HT
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CacheFourWay));
+            this.cacheImageBlock1 = new System.Windows.Forms.TableLayoutPanel();
+            this.cacheImageBlock2 = new System.Windows.Forms.TableLayoutPanel();
+            this.cacheImageBlock3 = new System.Windows.Forms.TableLayoutPanel();
+            this.cacheImageBlock4 = new System.Windows.Forms.TableLayoutPanel();
+            this.incomingAddressPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.multBox = new System.Windows.Forms.RichTextBox();
+            this.hitTextBox = new System.Windows.Forms.RichTextBox();
+            this.Set0_Entry0_valid = new System.Windows.Forms.Label();
+            this.Set0_Entry0_tag = new System.Windows.Forms.Label();
+            this.Set0_Entry0_index = new System.Windows.Forms.Label();
+            this.cacheImageBlock1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // cacheImageBlock1
+            // 
+            this.cacheImageBlock1.ColumnCount = 3;
+            this.cacheImageBlock1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.21053F));
+            this.cacheImageBlock1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.78947F));
+            this.cacheImageBlock1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.cacheImageBlock1.Controls.Add(this.Set0_Entry0_valid, 0, 0);
+            this.cacheImageBlock1.Controls.Add(this.Set0_Entry0_tag, 1, 0);
+            this.cacheImageBlock1.Controls.Add(this.Set0_Entry0_index, 2, 0);
+            this.cacheImageBlock1.Location = new System.Drawing.Point(29, 105);
+            this.cacheImageBlock1.Name = "cacheImageBlock1";
+            this.cacheImageBlock1.RowCount = 4;
+            this.cacheImageBlock1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.cacheImageBlock1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.cacheImageBlock1.Size = new System.Drawing.Size(174, 131);
+            this.cacheImageBlock1.TabIndex = 0;
+            // 
+            // cacheImageBlock2
+            // 
+            this.cacheImageBlock2.ColumnCount = 3;
+            this.cacheImageBlock2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.62832F));
+            this.cacheImageBlock2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.37168F));
+            this.cacheImageBlock2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.cacheImageBlock2.Location = new System.Drawing.Point(226, 105);
+            this.cacheImageBlock2.Name = "cacheImageBlock2";
+            this.cacheImageBlock2.RowCount = 4;
+            this.cacheImageBlock2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.cacheImageBlock2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.cacheImageBlock2.Size = new System.Drawing.Size(172, 131);
+            this.cacheImageBlock2.TabIndex = 1;
+            // 
+            // cacheImageBlock3
+            // 
+            this.cacheImageBlock3.ColumnCount = 3;
+            this.cacheImageBlock3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.03448F));
+            this.cacheImageBlock3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.96552F));
+            this.cacheImageBlock3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.cacheImageBlock3.Location = new System.Drawing.Point(423, 105);
+            this.cacheImageBlock3.Name = "cacheImageBlock3";
+            this.cacheImageBlock3.RowCount = 4;
+            this.cacheImageBlock3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.cacheImageBlock3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.cacheImageBlock3.Size = new System.Drawing.Size(171, 131);
+            this.cacheImageBlock3.TabIndex = 2;
+            // 
+            // cacheImageBlock4
+            // 
+            this.cacheImageBlock4.ColumnCount = 3;
+            this.cacheImageBlock4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.17391F));
+            this.cacheImageBlock4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.82609F));
+            this.cacheImageBlock4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.cacheImageBlock4.Location = new System.Drawing.Point(620, 105);
+            this.cacheImageBlock4.Name = "cacheImageBlock4";
+            this.cacheImageBlock4.RowCount = 4;
+            this.cacheImageBlock4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.cacheImageBlock4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.cacheImageBlock4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.cacheImageBlock4.Size = new System.Drawing.Size(168, 131);
+            this.cacheImageBlock4.TabIndex = 3;
+            // 
+            // incomingAddressPanel
+            // 
+            this.incomingAddressPanel.ColumnCount = 3;
+            this.incomingAddressPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.88754F));
+            this.incomingAddressPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.11246F));
+            this.incomingAddressPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.incomingAddressPanel.Location = new System.Drawing.Point(55, 9);
+            this.incomingAddressPanel.Name = "incomingAddressPanel";
+            this.incomingAddressPanel.RowCount = 1;
+            this.incomingAddressPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.incomingAddressPanel.Size = new System.Drawing.Size(350, 35);
+            this.incomingAddressPanel.TabIndex = 4;
+            // 
+            // multBox
+            // 
+            this.multBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.multBox.Location = new System.Drawing.Point(326, 386);
+            this.multBox.Name = "multBox";
+            this.multBox.ReadOnly = true;
+            this.multBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.multBox.Size = new System.Drawing.Size(72, 21);
+            this.multBox.TabIndex = 5;
+            this.multBox.Text = "    MULT";
+            // 
+            // hitTextBox
+            // 
+            this.hitTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hitTextBox.Location = new System.Drawing.Point(97, 418);
+            this.hitTextBox.Name = "hitTextBox";
+            this.hitTextBox.ReadOnly = true;
+            this.hitTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.hitTextBox.Size = new System.Drawing.Size(64, 20);
+            this.hitTextBox.TabIndex = 6;
+            this.hitTextBox.Text = "     HIT";
+            // 
+            // Set0_Entry0_valid
+            // 
+            this.Set0_Entry0_valid.AutoSize = true;
+            this.Set0_Entry0_valid.Location = new System.Drawing.Point(3, 0);
+            this.Set0_Entry0_valid.Name = "Set0_Entry0_valid";
+            this.Set0_Entry0_valid.Size = new System.Drawing.Size(32, 34);
+            this.Set0_Entry0_valid.TabIndex = 0;
+            this.Set0_Entry0_valid.Text = "Set0_Entry0_valid";
+            // 
+            // Set0_Entry0_tag
+            // 
+            this.Set0_Entry0_tag.AutoSize = true;
+            this.Set0_Entry0_tag.Location = new System.Drawing.Point(42, 0);
+            this.Set0_Entry0_tag.Name = "Set0_Entry0_tag";
+            this.Set0_Entry0_tag.Size = new System.Drawing.Size(65, 26);
+            this.Set0_Entry0_tag.TabIndex = 1;
+            this.Set0_Entry0_tag.Text = "Set0_Entry0_tag";
+            // 
+            // Set0_Entry0_index
+            // 
+            this.Set0_Entry0_index.AutoSize = true;
+            this.Set0_Entry0_index.Location = new System.Drawing.Point(116, 0);
+            this.Set0_Entry0_index.Name = "Set0_Entry0_index";
+            this.Set0_Entry0_index.Size = new System.Drawing.Size(54, 26);
+            this.Set0_Entry0_index.TabIndex = 2;
+            this.Set0_Entry0_index.Text = "Set0_Entry0_index";
             // 
             // CacheFourWay
             // 
@@ -39,14 +180,34 @@ namespace Project2_HT
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hitTextBox);
+            this.Controls.Add(this.multBox);
+            this.Controls.Add(this.incomingAddressPanel);
+            this.Controls.Add(this.cacheImageBlock4);
+            this.Controls.Add(this.cacheImageBlock3);
+            this.Controls.Add(this.cacheImageBlock2);
+            this.Controls.Add(this.cacheImageBlock1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CacheFourWay";
-            this.Text = "Form1";
+            this.Text = "MULT";
+            this.cacheImageBlock1.ResumeLayout(false);
+            this.cacheImageBlock1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel cacheImageBlock1;
+        private System.Windows.Forms.TableLayoutPanel cacheImageBlock2;
+        private System.Windows.Forms.TableLayoutPanel cacheImageBlock3;
+        private System.Windows.Forms.TableLayoutPanel cacheImageBlock4;
+        private System.Windows.Forms.TableLayoutPanel incomingAddressPanel;
+        private System.Windows.Forms.RichTextBox multBox;
+        private System.Windows.Forms.RichTextBox hitTextBox;
+        private System.Windows.Forms.Label Set0_Entry0_valid;
+        private System.Windows.Forms.Label Set0_Entry0_tag;
+        private System.Windows.Forms.Label Set0_Entry0_index;
     }
 }
