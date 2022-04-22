102a6dc68fb02c25aa28aca79a0f5088c67c334a
Nataliya Chibizova <60976901+nchibizova@users.noreply.github.com>
2022-04-22 19:32:15 -0400

    FormCall

--- Project3_HT/MemUnit.cs
+++ Project3_HT/MemUnit.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Project2_HT;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Project3_HT
 {
@@ -11,20 +13,25 @@ namespace Project3_HT
 
         public MemUnit(string name) : base(name)
         {
+            // call the cache form 
+            CacheFourWay cache = new CacheFourWay();
+            cache.Show();
         }
 
-       /* public override void Enqueue(Instruction instr)
-        {
-            if (Instructions.Count > 0)
-            {
-                throw new InvalidOperationException("Functional units can only execute one Instruction per cycle." +
-                    "\nPlease Dequeue the instruction or stall execution.");
-            }
-            Instructions.Enqueue(instr);
-            ExecTime = CalcExecutionTime(instr);
+        /* public override void Enqueue(Instruction instr)
+         {
+             if (Instructions.Count > 0)
+             {
+                 throw new InvalidOperationException("Functional units can only execute one Instruction per cycle." +
+                     "\nPlease Dequeue the instruction or stall execution.");
+             }
+             Instructions.Enqueue(instr);
+             ExecTime = CalcExecutionTime(instr);
 
-            Empty = false;
-        }*/
+             Empty = false;
+         }*/
+
+       
 
 
     }
