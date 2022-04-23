106409cac71c45f6067ccfe6ce370e29a02e41ca
MarlowAvery <45161056+MarlowAvery@users.noreply.github.com>
2022-04-22 20:55:32 -0400

    pushing some skeletons of the fp exec methods
    needs testing, i 100% know theres some stuff that can not stand atm

--- Project3_HT/FPU.cs
+++ Project3_HT/FPU.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Project3_HT;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,32 @@ namespace Project2_HT
 {
     class FPU
     {
+        public static float FADD(Instruction ins)
+        {
+            return ins.FReg1Data + ins.FReg2Data;
+        }
+        public static float FADDI(Instruction instr)
+        {
+            float.TryParse(instr.Imm, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out float tempImm);
+            return instr.FReg1Data + tempImm;
+        }
+        public static float FSUB(Instruction ins)
+        {
+            return ins.FReg1Data - ins.FReg2Data;
+        }
+        public static float FSUBI(Instruction ins)
+        {
+            float.TryParse(ins.Imm, NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out float tempImm);
+            return ins.Reg1Data - tempImm;
+        }
+        public static float FMULT(Instruction ins)
+        {
+            return ins.FReg1Data * ins.FReg2Data;
 
-
+        }
+        public static float FDIV(Instruction ins)
+        {
+            return ins.FReg1Data * ins.FReg2Data;
+        }
     }
 }
